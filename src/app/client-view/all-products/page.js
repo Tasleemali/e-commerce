665d0a3f397496186929cea7101ee40b69f21b0a
Tasleemali <133391205+Tasleemali@users.noreply.github.com>
2025-02-09 21:19:37 +0530

    Update page.js

--- src/app/client-view/all-products/page.js
+++ src/app/client-view/all-products/page.js
@@ -5,14 +5,14 @@ import { getDataAction } from '@/action'
 import Products from './products/page'
 
  async function AllProducts() {
-   const product = await getDataAction()
+   const product = await getDataAction();
 
   return (
     <div>
       
       <Products product={product}/>
     </div>
-  )
+  );
 }
 
 export default AllProducts
